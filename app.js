7705c14b4673641c6f8a24f90c9162381bb5b47f
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-05-02 13:46:23 -0700

    changes

---
+++ app.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    })
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        
+    });
+}
+navSlide()
+// const app = () => {
+//     navSlide();
+// }
